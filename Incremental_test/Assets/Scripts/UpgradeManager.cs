48ff846bb41ed634cd4e92852cb30c9a5b74abe5
Lewis Wilson <lewis.wilson@clinithink.com>
2017-01-24 16:02:22 +0000

    Added a scrolly bar that changes colour. This is currently not working properly...

--- Incremental_test/Assets/Scripts/UpgradeManager.cs
+++ Incremental_test/Assets/Scripts/UpgradeManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class UpgradeManager : MonoBehaviour {
 
@@ -10,18 +11,32 @@ public class UpgradeManager : MonoBehaviour {
 	public int clickPower;
 	public string itemName;
 	private float baseCost;
+    public Color unaffordable;
+    public Color affordable;
+    private Slider _slider;
 
 
 
 	// Use this for initialization
 	void Start () {
 		baseCost = cost;
+        _slider = GetComponentInChildren<Slider>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		itemInfo.text = itemName + "\nCost: " + cost + "\nPower: +" + clickPower;
-	}
+
+        _slider.value = click.gold / cost * 100;
+            if (_slider.value >= 100)
+            {
+                GetComponent<Image>().color = affordable;
+            }
+            else
+            {
+                GetComponent<Image>().color = unaffordable;
+            }
+    }
 
 	public void PurchasedUprade () {
 		if (click.gold >= cost) {
